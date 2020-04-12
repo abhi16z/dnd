1d508bec830039d03a574002bb28df27dd3cc2c4
MMT8048 <abhishek.kumar4@go-mmt.com>
2020-04-13 01:36:28 +0530

    enhancements and new item add

--- src/TodoReplace/TodoReplace.js
+++ src/TodoReplace/TodoReplace.js
@@ -1,86 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoContext from './todos.context';
 import './todoReplace.css';
 
 import * as STATUS from './status.constants';
 import Views from './Views';
 
+import { getDataFromLocalStorage, saveDataInLocalStorage } from './localStorage.utils';
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 const defaultData = {
-  [STATUS.TODO.value]: [
-    {
-      id: 1,
-      description: 'todo 1',
-      value: STATUS.TODO.value
-    },
-    {
-      id: 2,
-      description: 'todo 2',
-      value: STATUS.TODO.value
-    }
-  ],
-  [STATUS.BLOCKED.value]: [
-    {
-      id: 3,
-      description: 'todo 3',
-      value: STATUS.BLOCKED.value
-    },
-    {
-      id: 4,
-      description: 'todo 4',
-      value: STATUS.BLOCKED.value
-    }
-  ],
-  [STATUS.IN_PROGRESS.value]: [
-    {
-      id: 5,
-      description: 'todo 5',
-      value: STATUS.IN_PROGRESS.value
-    },
-    {
-      id: 6,
-      description: 'todo 6',
-      value: STATUS.IN_PROGRESS.value
-    }
-  ],
-  [STATUS.IN_REVIEW.value]: [
-    {
-      id: 7,
-      description: 'todo 7',
-      value: STATUS.IN_REVIEW.value
-    },
-    {
-      id: 8,
-      description: 'todo 8',
-      value: STATUS.IN_REVIEW.value
-    }
-  ],
-  [STATUS.DONE.value]: [
-    {
-      id: 9,
-      description: 'todo 9',
-      value: STATUS.DONE.value
-    },
-    {
-      id: 10,
-      description: 'todo 10',
-      value: STATUS.DONE.value
-    }
-  ]
+  [STATUS.TODO.value]: [],
+  [STATUS.BLOCKED.value]: [],
+  [STATUS.IN_PROGRESS.value]: [],
+  [STATUS.IN_REVIEW.value]: [],
+  [STATUS.DONE.value]: []
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export default function TodoReplace() {
   const [todoList, setTodoList] = useState(defaultData);
   const [isAddActive, toggleActive] = useState(false);
+  const [newDesc, setNewDesc] = useState('');
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  useEffect(() => {
+    const data = getDataFromLocalStorage() || {}; // if null then default {}
+    const keys = Object.keys(data);
+    if (keys.includes(STATUS.BLOCKED.value) && keys.includes(STATUS.TODO.value) && keys.includes(STATUS.IN_PROGRESS.value) && keys.includes(STATUS.IN_REVIEW.value) && keys.includes(STATUS.DONE.value)) {
+      setTodoList(data);
+    }
+  }, []);
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   const handleAddNew = () => {
+    const newTodo = {
+      id: (new Date()).getTime(),
+      description: newDesc,
+      value: STATUS.TODO.value
+    };
 
+    const tempTodoList = {
+      ...todoList
+    };
+
+    tempTodoList[STATUS.TODO.value].push(newTodo);
+
+    setTodoList(tempTodoList);
+    setNewDesc('');
+    toggleActive(false);
+    saveDataInLocalStorage(tempTodoList);
   };
 
-  /////////////////////////////////////////////////////////////////////////////
-  const updateTodo = ({todoItem, draggedTo}) => {
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const updateTodo = ({ todoItem, draggedTo }) => {
     const oldStatusValue = todoItem.value;
     const newStatusValue = draggedTo;
-    
+
     const itemsInOldStatus = todoList[oldStatusValue].filter(item => item.id !== todoItem.id);
     const itemsInDraggedStatus = todoList[newStatusValue];
     itemsInDraggedStatus.push({
@@ -88,12 +69,17 @@ export default function TodoReplace() {
       value: newStatusValue
     });
 
-    setTodoList({
+    const tempTodoLost = {
       ...todoList,
       [oldStatusValue]: itemsInOldStatus,
       [newStatusValue]: itemsInDraggedStatus
-    });
+    };
+
+    setTodoList(tempTodoLost);
+    saveDataInLocalStorage(tempTodoLost);
   };
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   const handleDragOver = (ev) => {
     ev.preventDefault();
@@ -103,14 +89,16 @@ export default function TodoReplace() {
   const handleDrop = (statusValue) => (ev) => {
     ev.preventDefault();
     const todoItem = JSON.parse(ev.dataTransfer.getData('text/plain'));
-    updateTodo({todoItem, draggedTo: statusValue});
+    updateTodo({ todoItem, draggedTo: statusValue });
   };
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <TodoContext.Provider value={{ todoList, updateTodo }}>
       <div style={{ height: window.innerHeight + 500 }}>
         <div className="header">
-          TO DO App
+          TASK LIST
         </div>
         <div className="view-container">
           <Views
@@ -129,6 +117,9 @@ export default function TodoReplace() {
             showAdd
             isAddActive={isAddActive}
             toggleActive={toggleActive}
+            newDesc={newDesc}
+            setNewDesc={setNewDesc}
+            handleAddNew={handleAddNew}
           />
           <Views
             header="In Progress"
--- src/TodoReplace/Views.js
+++ src/TodoReplace/Views.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 export default function TodoView(props) {
-  const { header, className, handleDragOver, handleDrop, todoItems, showAdd, isAddActive, toggleActive } = props;
+  const { header, className, handleDragOver, handleDrop, todoItems, showAdd, isAddActive, toggleActive, newDesc, setNewDesc, handleAddNew } = props;
 
   const onDragStart = (todoItem) => (ev) => {
     const dataTransfer = ev.dataTransfer;
     dataTransfer.setData('text/plain', JSON.stringify(todoItem));
   };
 
+  const onCancel = () => {
+    setNewDesc('')
+    toggleActive(false);
+  };
+
   return (
     <div className={className} onDragOver={handleDragOver} onDrop={handleDrop}>
-      <div className="status-header">{header}</div>
+      <div className="status-header" >{header}</div>
       <div>
         {todoItems.map((todoItem) => {
           return (
@@ -20,13 +25,13 @@ export default function TodoView(props) {
           );
         })}
       </div>
-      {showAdd && !isAddActive && <button className="add-button" onClick={() => toggleActive(true)}>Add New</button>}
+      {showAdd && !isAddActive && <button className="primary-button" onClick={() => toggleActive(true)}>Add New</button>}
       {showAdd && isAddActive && (
         <div className="add-input-container">
-          <textarea />
+          <textarea value={newDesc} onChange={ev => setNewDesc(ev.target.value)} autoFocus />
           <div className="add-input-button-container">
-            <button className="cancel-button" onClick={() => toggleActive(true)}>Cancel</button>
-            <button className="add-input-button" onClick={() => toggleActive(true)}>Add</button>
+            <button className="cancel-button" onClick={onCancel}>Cancel</button>
+            <button className="add-input-button" onClick={handleAddNew}>Add</button>
           </div>
         </div>
       )}
